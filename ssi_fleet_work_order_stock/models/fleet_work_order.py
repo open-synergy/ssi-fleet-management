52aa4050a3a9b5c3af98d41039b8effe6935f1ce
Andhitia Rama <andhitia.r@gmail.com>
2023-08-21 05:36:29 +0700

    [QC Functional]

--- ssi_fleet_work_order_stock/models/fleet_work_order.py
+++ ssi_fleet_work_order_stock/models/fleet_work_order.py
@@ -9,11 +9,12 @@ class FleetWorkOrder(models.Model):
     _name = "fleet_work_order"
     _inherit = ["fleet_work_order"]
 
-    picking_type_id = fields.Many2one(
-        comodel_name="stock.picking.type",
-        string="Picking Type",
-        required=True,
-        ondelete="restrict",
+    picking_ids = fields.Many2many(
+        comodel_name="stock.picking",
+        string="Picking",
+        relation="rel_fleet_work_order_2_picking",
+        column1="wo_id",
+        column2="picking_id",
         readonly=True,
         states={"draft": [("readonly", False)]},
     )
@@ -21,5 +22,5 @@ class FleetWorkOrder(models.Model):
         comodel_name="stock.picking.type",
         string="Allowed Picking Types",
         related="type_id.allowed_picking_type_ids",
-        store=False
+        store=False,
     )
